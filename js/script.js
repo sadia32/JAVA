c61193a3ab6a454490d1f80dc53b34401433a998
sadia32 <80625386+sadia32@users.noreply.github.com>
2022-08-07 23:10:59 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,46 @@
+let firstName='Sadia';
+let lastName=prompt('what is your last name?');
+let age=prompt('what is your age?');
+
+
+
+document.getElementById('html').innerHTML= firstName + ' '+ lastName + ' . ' +' '+ 'i am '+age+'years old';
+
+console.log(firstName + ' '+ lastName + ' . ' +' '+ 'i am '+age+'years old');
+
+
+
+
+
+
+/*var x,y;
+var x=10;
+var y=90;
+
+z=x*y;
+document.getElementById('html').innerHTML= z;*/
+ 
+/*console.log()  
+
+
+let aliage=29;
+aliage+=5
+let shuvoage=28;
+let totalage=aliage+shuvoage;
+document.getElementById('html').innerHTML= totalage;
+
+
+console.log(shuvoage);*/
+
+
+
+
+
+
+
+ 
+
+
+
+
+
